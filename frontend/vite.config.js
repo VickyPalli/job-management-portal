437047fe0355be8536c51fa9e9b94f84f0731a2e
VickyPalli <pallivenkaiah897@gmail.com>
2025-03-25 20:11:00 +0530

    final1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),   tailwindcss(),
-  ],
-
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Allows access from external networks
+    port: process.env.PORT || 5173, // Uses the environment variable or defaults to 5173
+    strictPort: true,
+  },
+});
